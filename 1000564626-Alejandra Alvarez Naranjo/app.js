ed812ccdc97c2156f319271ef481677771e6e866
aleja.nrj <Alejanrj.07@gmail.com>
2025-09-27 14:24:03 -0500

    Fase 3: Añadida simulación semanal y diagnóstico financiero

--- 1000564626-Alejandra Alvarez Naranjo/app.js
+++ 1000564626-Alejandra Alvarez Naranjo/app.js
@@ -1,16 +1,14 @@
 let nombreUsuario = prompt("Ingrese su nombre de usuario");
-let saldo = 1000000; 
+let saldo = 1000;
 let gastosTotales = 0;
-
-alert("Bienvenido " + nombreUsuario + " a su banca en línea"); 
 
 function mostrarBalance() {
     console.log(`Hola, ${nombreUsuario}, su saldo actual es de: $${saldo}`);
 }
 
-const registrarIngreso = function(montoIngreso) {
-    console.log(`Se ha registrado un ingreso de: $${montoIngreso}`);
+const registrarIngreso = function (montoIngreso) {
     saldo += montoIngreso;
+    console.log(`Se ha registrado un ingreso de: $${montoIngreso}`);
     console.log(`Su nuevo saldo es de: $${saldo}`);
 }
 
@@ -25,3 +23,38 @@ const registrarGasto = (montoGasto) => {
         console.log(" Fondos insuficientes");
     }
 };
+const simularSemana = () => {
+
+
+    for (let dia = 1; dia < 8; dia++) {
+        console.log(`Día ${dia}:`);
+        registrarGasto(50);
+    }
+};
+simularSemana();
+
+
+function diagnosticoFinanciero() {
+    if (gastosTotales > 300) {
+        console.log("Tu salud financiera es excelente.");
+    } else {
+        console.log("Alerta: Revisa tus gastos, podrías estar gastando demasiado.");
+    }
+}
+function diagnosticoFinanciero() {
+    switch (true) {
+        case gastosTotales > 300:
+            console.log("excedes los gastos");
+            break;
+        case gastosTotales > 150:
+            console.log("gasta moderadamente");
+            break;
+        default:
+            console.log("has sido muy prudente con tus gastos");
+            break;
+    }
+}
+
+
+
+
