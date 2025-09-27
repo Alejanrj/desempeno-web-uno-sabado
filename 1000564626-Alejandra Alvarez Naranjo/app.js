0b6d7a3fd636722e67230fa8b2555d7c41123da6
aleja.nrj <Alejanrj.07@gmail.com>
2025-09-27 13:32:06 -0500

    Fase 2: Implementadas funciones para ingresos y gastos

--- 1000564626-Alejandra Alvarez Naranjo/app.js
+++ 1000564626-Alejandra Alvarez Naranjo/app.js
@@ -1,10 +1,27 @@
-nombreUsuario = prompt("Ingrese su nombre de usuario");
-saldo = 1000000; 
-gastosTotales = 0;
+let nombreUsuario = prompt("Ingrese su nombre de usuario");
+let saldo = 1000000; 
+let gastosTotales = 0;
 
 alert("Bienvenido " + nombreUsuario + " a su banca en línea"); 
 
 function mostrarBalance() {
-    console.log("Hola, [nombreUsuario], su saldo actual es de: $[saldo]");
+    console.log(`Hola, ${nombreUsuario}, su saldo actual es de: $${saldo}`);
 }
-mostrarBalance();
+
+const registrarIngreso = function(montoIngreso) {
+    console.log(`Se ha registrado un ingreso de: $${montoIngreso}`);
+    saldo += montoIngreso;
+    console.log(`Su nuevo saldo es de: $${saldo}`);
+}
+
+const registrarGasto = (montoGasto) => {
+    if (montoGasto <= saldo) {
+        saldo -= montoGasto;
+        gastosTotales += montoGasto;
+        console.log(`Gasto registrado correctamente: $${montoGasto}`);
+        console.log(`Saldo restante: $${saldo}`);
+        console.log(`Gastos totales: $${gastosTotales}`);
+    } else {
+        console.log(" Fondos insuficientes");
+    }
+};
